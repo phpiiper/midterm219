1b2719a77128518d1452621831c7793b153beafa
phpiiper <plp@njit.edu>
2024-03-25 19:26:14 -0400

    Added History Command

--- app/plugins/history/__init__.py
+++ app/plugins/history/__init__.py
@@ -1,7 +1,18 @@
 import logging
+import os
 from app.commands import Command
+import pandas as pd
 
 
 class HistoryCommand(Command):
     def execute(self):
-        print("HISTORY!")
+        data_dir = './data'
+        csv_file_path = os.path.join(data_dir, 'calc_history.csv')
+        try:
+            df_read_states = pd.read_csv(csv_file_path)
+            print("Calc History:")
+            for item in df_read_states.iterrows():
+                print(f"{item[1][0]}, {item[1][1]}")
+        except:
+            print("Unavailable")
+        
