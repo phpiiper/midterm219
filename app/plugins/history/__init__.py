adf252c2ff5625a01503e7d9577f33ec870abd65
phpiiper <plp@njit.edu>
2024-03-28 16:16:10 -0400

    Fixed: history command

--- app/plugins/history/__init__.py
+++ app/plugins/history/__init__.py
@@ -2,17 +2,25 @@ import logging
 import os
 from app.commands import Command
 import pandas as pd
+from app import App
 
 
 class HistoryCommand(Command):
     def execute(self):
+        app = App()
         data_dir = './data'
-        csv_file_path = os.path.join(data_dir, 'calc_history.csv')
+        csv_file_path = os.path.join(data_dir, app.get_environment_variable("HISTORYFILENAME","history.csv"))
         try:
-            df_read_states = pd.read_csv(csv_file_path)
+            df_read = pd.read_csv(csv_file_path)
             print("Calc History:")
-            for item in df_read_states.iterrows():
-                print(f"{item[1][0]}, {item[1][1]}")
+            logging.info(f"Printed History")
+            print("Operation, Values")
+            logging.info(f"Operation, Values")
+            oper_list = [list(item) for item in df_read.values] if not df_read.empty else []
+            for item in oper_list:
+                print(f" > {item[0]}, {item[1]}")
+                logging.info(f" > {item[0]}, {item[1]}")
         except:
-            print("Unavailable")
+            print("History is unavailable.")
+
         
