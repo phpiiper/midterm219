623a75e059f97f1c1fc3c059d301eec70ae58020
phpiiper <plp@njit.edu>
2024-03-28 16:17:07 -0400

    Tidied up save function

--- app/save/__init__.py
+++ app/save/__init__.py
@@ -6,7 +6,8 @@ import json
 
 
 def SaveOperation(operation):
-    data_dir = './data'
+    app = App()
+    data_dir = app.get_environment_variable("DATADIRNAME",'./data')
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
         logging.info(f"The directory '{data_dir}' is created.")
@@ -14,7 +15,8 @@ def SaveOperation(operation):
         logging.error(f"The directory '{data_dir}' is not writable.")
         return
     
-    csv_file_path = os.path.join(data_dir, 'calc_history.csv')
+    fn = app.get_environment_variable("HISTORYFILENAME","history.csv")
+    csv_file_path = os.path.join(data_dir, fn)
     oper_list = []
     if os.path.exists(csv_file_path):
         df_read = pd.read_csv(csv_file_path)
@@ -22,7 +24,6 @@ def SaveOperation(operation):
             oper_list = [list(item) for item in df_read.values] if not df_read.empty else []
     if operation:
         oper_list.append([operation[0],operation[1]])
-    app = App()
     cols = json.loads(app.get_environment_variable("DATACOLUMNS",["Oper.","Vars."]))
     df_data = pd.DataFrame(oper_list, columns=cols)
     df_data.to_csv(csv_file_path, index=False)
